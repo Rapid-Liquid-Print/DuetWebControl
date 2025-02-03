3fcf45de0a3ef400d185d75fe598547ab81d0250
ArianeSchoenwiesner <ariane@rapidliquidprint.co>
2025-02-03 13:16:43 -0500

    update extrusion amount

--- src/plugins/RLP/RLP.vue
+++ src/plugins/RLP/RLP.vue
@@ -1097,8 +1097,8 @@ export default Vue.extend ({
 			else if (this.needle == 0.5) {
 				aS = -1*(4.4 * (tempRat/(tempRat+1)));
 				bS = -1*(4.4 / (tempRat+1));
-				console.log(this.makeExtrusionString(160));
-				await this.sendCode("G1 "+this.makeExtrusionString(160));
+				console.log(this.makeExtrusionString(200));
+				await this.sendCode("G1 "+this.makeExtrusionString(200));
 				await this.sendCode("G1 E"+String(aS)+":"+String(bS));
 			}
 			await this.sendCode("M400");
